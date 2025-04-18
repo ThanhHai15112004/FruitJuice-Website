06d8d34927019612f26da6d125c9dae1102738d6
Phan Thanh Hải <143269710+ThanhHai15112004@users.noreply.github.com>
2025-04-18 17:31:52 +0700

    up

--- asset/scripts/main.js
+++ asset/scripts/main.js
@@ -1,11 +1,23 @@
-function toggleMenu() {
+//on off menu
+
+function toggleMenu(forceClose = false) {
   const nav = document.getElementById("mainNav");
   const overlay = document.getElementById("headerOverlay");
-  nav.classList.toggle("show");
-  overlay.classList.toggle("show");
+
+  if (forceClose || nav.classList.contains("show")) {
+    nav.classList.remove("show");
+    overlay.classList.remove("show");
+  } else {
+    nav.classList.add("show");
+    overlay.classList.add("show");
+  }
 }
 
-function toggleSubmenu() {
-  const nav = document.getElementById("mainNav");
-  nav.classList.toggle("show");
-}
+// Bắt sự kiện click ra ngoài menu
+document.addEventListener("DOMContentLoaded", function () {
+  const overlay = document.getElementById("headerOverlay");
+
+  overlay.addEventListener("click", () => {
+    toggleMenu(true); // Force đóng
+  });
+});
