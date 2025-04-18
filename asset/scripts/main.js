7194ddd748ea0fda6a0d1501567e78fa26b82152
Phan Thanh Hải <143269710+ThanhHai15112004@users.noreply.github.com>
2025-04-18 16:58:06 +0700

    main

---
+++ asset/scripts/main.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const nav = document.getElementById("mainNav");
+    const overlay = document.getElementById("headerOverlay");
+  
+    nav.classList.toggle("show");
+    overlay.classList.toggle("show");
+
+    nav.classList.toggle("show");
+  }
+  
